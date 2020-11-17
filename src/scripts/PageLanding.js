70aa2acaa561df8f9e87a67d270bc99d7bc26bd2
Felipe-CdS <felipe32santos@hotmail.com>
2020-11-17 18:19:27 -0300

    minor changes & fix'd bugs

--- src/scripts/PageLanding.js
+++ src/scripts/PageLanding.js
@@ -17,16 +17,29 @@ function getEntries(){
 }
 
 function checkButton(){
-    console.log("checking");
     if(document.getElementById("fname").value.length != 0 && 
         document.getElementById("lname").value.length != 0 && 
-        document.getElementById("email").value.length != 0){
-
-        console.log("check");
+        document.getElementById("email").value.length != 0 &&
+        validateEmail() == true){
+        
         document.getElementById("submit-button").removeAttribute("disabled");
     }
     else{ 
         document.getElementById("submit-button").setAttribute("disabled", true);
+    }
+}
+
+function validateEmail(){
+    var regex = /^\S+@\S+\.\S+$/;
+    var emailEntry = document.getElementById("email").value;
+
+    if(regex.test(emailEntry)){
+        document.getElementById("email-error").style.visibility = "hidden";
+        return true;
+    }
+    else{
+        document.getElementById("email-error").style.visibility = "visible";
+        return false;
     }
 }
 
@@ -42,6 +55,8 @@ function clearEverything(){
 document.getElementById("fname").addEventListener("change", checkButton);
 document.getElementById("lname").addEventListener("change", checkButton);
 document.getElementById("email").addEventListener("change", checkButton);
+
+document.getElementById("email").addEventListener("change", validateEmail);
 
 document.getElementById("entries-form").addEventListener("submit", getEntries);
 document.getElementById("cancel-button").addEventListener("click", clearEverything);
